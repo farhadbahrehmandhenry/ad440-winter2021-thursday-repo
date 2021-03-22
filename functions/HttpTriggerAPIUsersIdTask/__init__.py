fae8b39690680a8f9053714f160b73666802edf9
Farhad <fbahrehmand82@gmail.com>
2021-03-22 16:01:50 -0700

    run action

--- functions/HttpTriggerAPIUsersIdTask/__init__.py
+++ functions/HttpTriggerAPIUsersIdTask/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 import datetime
 import redis 
 
-redisFeature = "On" == os.environ["CACHE_TOGGLE"]
+redisFeature = os.environ["CACHE_TOGGLE"]
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info(
@@ -29,7 +29,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         logging.debug("Error: " + e.args[1])
         return func.HttpResponse(status_code=500)
     logging.debug("Connected to DB successfuly!")
-
+    rDB = None
 
     #Redis Sever
 
